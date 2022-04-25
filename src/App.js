0195ffe16fcb8f0fa5d9a1fc47bc947deb7df1cf
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-04-25 18:29:14 -0300

    Conclui requisito 1, cria componentes, e utiliza BR.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Album from './Album';
+import Favorites from './Favorites';
+import Login from './Login';
+import NotFound from './NotFound';
+import Profile from './Profile';
+import ProfileEdit from './ProfileEdit';
+import Search from './Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes!</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+
+      </div>
+    );
   }
 }
 
